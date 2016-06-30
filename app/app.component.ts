c5cef9f990e389c87d190b072c67a4a1f9f35095
tperroin <thibault.perroin@gmail.com>
2016-06-30 17:15:20 +0200

    angular2 starter

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by t.perroin on 30/06/2016.
+ */
+import { Component } from '@angular/core';
+@Component({
+    selector: 'geniustest',
+    template: '<h1>First GeniusTest app</h1>'
+})
+export class AppComponent { }
---
+++ app/main.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by t.perroin on 30/06/2016.
+ */
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+bootstrap(AppComponent);
