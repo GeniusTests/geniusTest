aba44ce00db363238f92106241555b5a46f29424
tperroin <thibault.perroin@gmail.com>
2016-07-21 17:25:06 +0200

    first angular2 stuff

---
+++ app/components/feed/feed.component.ts
@@ -0,0 +1,3 @@
+/**
+ * Created by t.perroin on 21/07/2016.
+ */
---
+++ app/components/project/project.menu.component.ts
@@ -0,0 +1,48 @@
+/**
+ * Created by t.perroin on 21/07/2016.
+ */
+/**
+ * Created by t.perroin on 21/07/2016.
+ */
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'div[project-menu]',
+    template: `
+        <div class="right floated left aligned three wide column">
+            <div class="ui small inverted grey vertical menu">
+                <div class="active item">
+                    Your projects
+                </div>
+                <div class="item">
+                    Home
+                    <div class="menu">
+                        <a class="item">Search</a>
+                        <a class="item">Add</a>
+                        <a class="item">Remove</a>
+                    </div>
+                </div>
+                <a class="item"><i class="grid layout icon"></i> Browse </a>
+                <a class="item">Messages </a>
+                <div class="ui dropdown item">More <i class="dropdown icon"></i>
+                    <div class="menu">
+                        <a class="item"><i class="edit icon"></i> Edit Profile</a>
+                        <a class="item"><i class="globe icon"></i> Choose Language</a>
+                        <a class="item"><i class="settings icon"></i> Account Settings</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+})
+export class FeedComponent {
+    /*heroes:Hero[] = [];
+
+     constructor(private heroService:HeroService) {
+     }
+
+     ngOnInit() {
+     this.heroService.getHeroes()
+     .then(heroes => this.heroes = heroes);
+     }*/
+}
